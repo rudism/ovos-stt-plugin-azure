563c471bf9c99e61384ea07259fea69a85b91b48
Rudis Muiznieks <rudis@sitosis.com>
2026-05-22 17:39:13 -0500

    logging

--- ovos_stt_plugin_azure/__init__.py
+++ ovos_stt_plugin_azure/__init__.py
@@ -3,25 +3,29 @@ from ovos_plugin_manager.stt import STT
 from ovos_utils import classproperty
 from ovos_plugin_manager.utils.audio import AudioData, AudioFile
 from typing import Optional
+from ovos_utils.log import LOG
 
 
 class OVOSAzureSTT(STT):
     def __init__(self, config=None):
         super().__init__(config)
+        self.lang_override = self.config.get("lang")
         self.key = self.config.get("key")
         self.region = self.config.get("region", "westeurope")
         self.profanity = self.config.get("profanity", "raw")
 
     def execute(self, audio: AudioData, language: Optional[str]=None):
-        lang = language or self.lang
+        lang = self.lang_override or language or self.lang
         headers = {
             'Content-type': 'audio/wav;codec="audio/pcm";',
             'Ocp-Apim-Subscription-Key': self.key
         }
         url = f"https://{self.region}.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1"
-        response = requests.request("POST", url, headers=headers,
+        raw_response = requests.request("POST", url, headers=headers,
                                     data=audio.get_wav_data(),
-                                    params={"language": lang, "profanity": self.profanity}).json()
+                                    params={"language": lang, "profanity": self.profanity})
+        LOG.info(f"url: {url}, lang: {lang}, profanity: {self.profanity}, response: {raw_response.content}")
+        response = raw_response.json()
         return response["DisplayText"]
 
     @classproperty
